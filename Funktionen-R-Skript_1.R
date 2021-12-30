f164daf9afabe50f70596f7cf2ce996213a7b66b
louisap12 <94864182+louisap12@users.noreply.github.com>
2021-12-30 14:24:15 +0100

    Anfang Aufgabe 3 (a)
    Ich habe schon mal ein R-Dokument angefangen und 3(a) bearbeitet.
    Zuerst habe ich noch einen Testdatensatz zum Testen der Funktionen (zumindest (a) und (b)) erstellt.
    Ich habe die Ausgabe der Funktion mit cat() gemacht, vielleicht ist aber eine liste/dataframe übersichtlicher.
    Fallen euch noch weitere deskriptive Statistiken ein oder reichen die die drin sind? :)

---
+++ Funktionen-R-Skript_1.R
@@ -0,0 +1,30 @@
+## Funktionen-R-Skript 1
+## zu Aufgabe 3
+
+
+# Datensatz zum ausprobieren:
+# zunaechst mit einer metrischen (hier jetzt erstmal nur natuerliche Zahlen) und einer kategoriellen Spalte
+
+set.seed(12)
+test <- data.frame( met = sample(1:100 , 20 ), kat = sample( c("sehr gut","gut","schlecht"),20, replace=TRUE) )
+test
+
+
+# Die Ausgabe mit cat, alternativ vielleicht in data.frame, liste oder matrix ?
+sum_met <- function(x){
+   AM <- mean(x)
+   med <- median(x)
+   var <- var(x)
+   sd <- sd(x)
+   quartile <- quantile(x, probs= c(0.25,0.75) )
+   return({
+   cat("\n Mittelwerte:\n","Arithmetische Mittel:",AM,"\n" ,"Median:",med,"\n","------------") 
+   cat( "\n Streuungsmaße:\n","Varianz:",var,"\n","Standardabweichung:",sd,"\n","------------")
+   cat("\n Quartile\n","0.25-Quartil:",quartile[1],"\n","0.75-Quartil:",quartile[2] )
+   })
+   }
+
+sum_met(test$met)
+
+
+
