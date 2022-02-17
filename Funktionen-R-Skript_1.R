1b27b788cfad37b611d71050c1dcb7f6b7b62790
DayaLuna <94837691+DayaLuna@users.noreply.github.com>
2022-02-17 10:28:50 +0100

    Update Funktionen-R-Skript_1.R

--- Funktionen-R-Skript_1.R
+++ Funktionen-R-Skript_1.R
@@ -6,7 +6,10 @@
 # zunaechst mit einer metrischen (hier jetzt erstmal nur natuerliche Zahlen) und einer kategoriellen Spalte
 
 set.seed(12)
-test <- data.frame( met = sample(1:100 , 20 ), kat = sample( c("sehr gut","gut","schlecht"),20, replace=TRUE) )
+# ich ergänze das Test-Data Frame um eine kategorielle Variable
+test <- data.frame( met = sample(1:100 , 20 ), 
+                    kat = sample( c("sehr gut","gut","schlecht"),20, replace=TRUE),
+                    dich = sample(0:1, 20, replace = TRUE))
 test
 
 
