3d2c27c4d6d7b4fbfff9dad3bc2578fae4bd1948
DayaLuna <94837691+DayaLuna@users.noreply.github.com>
2022-02-17 11:07:55 +0100

    Update Funktionen-R-Skript_2.R

--- Funktionen-R-Skript_2.R
+++ Funktionen-R-Skript_2.R
@@ -2,9 +2,7 @@ dich_as_met <- function(x){
   return(as.numeric(factor(x)))
 }
 
-test <- data.frame( met = sample(1:100 , 20 ), 
-                    kat = sample( c("sehr gut","gut","schlecht"),20, replace=TRUE),
-                    dich = sample(0:1, 20, replace = TRUE))
+test2 <- sample(c("ja", "nein"), 20, replace = TRUE)
 
-dich_as_met(test$dich)
-# [1] 2 1 2 1 1 2 1 1 2 2 1 1 1 1 1 2 1 2 2 1
+dich_as_met(test2)
+# [1] 2 2 1 1 1 1 1 2 1 2 2 2 2 1 1 1 1 1 1 1
