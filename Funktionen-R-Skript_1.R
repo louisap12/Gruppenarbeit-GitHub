2803a1665376b8aacb39487ad342b39b1e85d53c
louisap12 <louisa.poggel@tu-dortmund.de>
2022-02-17 10:52:55 +0100

    Update Funktionen-R-Skript_1.R

--- Funktionen-R-Skript_1.R
+++ Funktionen-R-Skript_1.R
@@ -13,21 +13,20 @@ test <- data.frame( met = sample(1:100 , 20 ),
 test
 
 
-# Die Ausgabe mit cat, alternativ vielleicht in data.frame, liste oder matrix ?
+# a)
 sum_met <- function(x){
    AM <- mean(x)
    med <- median(x)
    var <- var(x)
    sd <- sd(x)
    quartile <- quantile(x, probs= c(0.25,0.75) )
-   return({
-   cat("\n Mittelwerte:\n","Arithmetische Mittel:",AM,"\n" ,"Median:",med,"\n","------------") 
-   cat( "\n Streuungsmaße:\n","Varianz:",var,"\n","Standardabweichung:",sd,"\n","------------")
-   cat("\n Quartile\n","0.25-Quartil:",quartile[1],"\n","0.75-Quartil:",quartile[2] )
-   })
+   return( list( "Arithmetische Mittel" = AM, "Median" = med, "Varianz" = var, "Standardabweichung" = sd,
+                 "Quartile"= quartile) )
    }
 
 sum_met(test$met)
+
+
 
 
 
